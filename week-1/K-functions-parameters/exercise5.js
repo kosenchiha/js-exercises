0375f3d97024569d646d5ee327d0d76dc7ae424a
Vicky <kosenchiha@gmail.com>
2019-03-03 22:57:42 +0000

    completed ex K-function-parameters

--- week-1/K-functions-parameters/exercise5.js
+++ week-1/K-functions-parameters/exercise5.js
@@ -1,5 +1,7 @@
 // Declare your function here
-
+function createLongGreeting(name, b) {
+  return "Hello, my name is " + name + " and I'm " + b + " years old";
+}
 const greeting = createLongGreeting("Daniel", 30);
 
 console.log(greeting);
