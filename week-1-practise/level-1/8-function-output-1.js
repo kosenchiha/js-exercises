67d709ec3e30d07b74a6a78958468aaa21ede96b
Vicky <kosenchiha@gmail.com>
2019-03-04 22:30:40 +0000

    completed exercises from Level-1

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -7,6 +7,10 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do?
+// It returns a rundom number in range 0-1(inclusive of 0, but not 1) and mulplies it by 10.
 // - How many parameters does the function take?
+// none
 // - What is the function name?
+// getNumber
 // - Where is the function called? with what parameters?
+// var result = getNumber(); with no parameters
--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -7,8 +7,14 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do? Run it and see, or Google some of the methods used ot understand them.
+//  it concatenates the string arguments to the calling string and returns a new string.
 // - How many parameters does the function take?
+// two
 // - What is the function name?
+// s
 // - Where is the function called? with what parameters?
+//  var result = s("code", "your"); two parameters: "code", "your"
 // - What does the function return?
+//  codeyour
 // - How can this function be improved?
+// return w1.concat(' ', w2) will add a space between strings
